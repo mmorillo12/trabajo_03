4c3e740e3981f26234d46fa6e679bcf4d1b81eab
mmorillo12 <78889380+mmorillo12@users.noreply.github.com>
2021-02-10 22:35:34 -0400

    Practicas para la semana 4

---
+++ trabajo3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace vscode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Practica 3
+        int numero = 101;
+        for(int i = 1;(i<=20)&&(numero > 100);i++){
+            if(numero%2==0){
+                Console.WriteLine(numero);
+            }
+            numero += 1;
+        }
+    } 
+}}
---
+++ trabajo4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace vscode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //Practica 4
+        
+        char letra; 
+        for(letra = 'Z'; letra >= 'A';letra--){
+            Console.WriteLine("{0}", letra);
+        }
+        }
+    } 
+}
