3dcc79f39bd2a9563445c0deafe2224a25a1270a
mmorillo12 <78889380+mmorillo12@users.noreply.github.com>
2021-02-10 22:26:51 -0400

    Inicio semana 04
    Espero que los trabajos esten correctos, sino estan asi, pues me gustaria saber como lo haria usted, cual seria la manera mas eficiente de como hacerlo y cuales fueron mis errores.

---
+++ practica#dos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace vscode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Practica 2
+         
+         int numero = 1;
+         do{
+            Console.WriteLine(numero);
+            numero += 1;
+         }while(numero <= 10);
+        }
+    }
+}
---
+++ practica#uno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace vscode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Practica 1
+           int valor,suma = 0;
+        Console.WriteLine("Bienvenido a su programa de adicion si desea dejar de sumar presione 0 o un numero negativo"); 
+           
+            do{ 
+            Console.Write("Ingrese sus numeros: ");
+            valor = int.Parse(Console.ReadLine());
+            if(valor < 0){
+              Console.WriteLine(suma);
+            }
+            suma += valor;
+            }
+            while((valor != 0)||(valor !< 0));
+            
+         Console.WriteLine(suma);
+        }
+    }
+}
